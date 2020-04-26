c295b892b9174c55d3e436e76de2ec381d2bd6fb
joladubu <joladubu@gmail.com>
2020-04-26 12:43:13 +0100

    Added routes and react files setup for project

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+// importing the User Model
+const User = require('../models/User');
 
 //@route   POST api/users
 //@desc    Register a user
 //@access  Public
-router.post('/', (req, res) => {
-  res.send('Register a User')
-})
+router.post('/', [
+  check('name', 'Please add name')
+    .not()
+    .isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], 
+  //req meaning request is the data that is sent to the route
+  (req, res) => {
+
+    //assigning validationResult to a variable errors passing req as a parameter
+    const errors = validationResult(req);
+
+    // code to check if the error returned is not validated
+    if(!errors.isEmpty()) {
+
+      //return error status of 400 displaying the error as an array
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // error returned is validated
+    res.send('passed')
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+// this file is the basic enry point to the App
 const express = require('express');
 
+const connectDB = require('./config/db');
+
+// initializing express
 const app = express();
 
-app.get('/', (req, res) => 
-  res.json({'msg': 'Contact keeper API'})
-);
+// Connect Database
+connectDB();
 
-//Define Routes
+// Init Middleware
+app.use(express.json({ extended: false }));
 
+// Defining  basic route for the api https://localhost:5000
+app.get('/', (req, res) => res.json({ msg: 'Contact keeper API' }));
+
+/*
+Define Routes files with app.use(): once the endpoint
+is hit, it would require the path to the file in require method
+*/
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
